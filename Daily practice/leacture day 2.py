73d1e7fda4879dca24c6120c6cab5b6b64d34cf6
aadityakun <aaditya27srivastava2009@gmail.com>
2026-06-23 20:40:39 +0530

    Add files via upload

---
+++ Daily practice/leacture day 2.py
@@ -0,0 +1,78 @@
+# str1 = "hleoo"
+# len1 = len(str1)
+# print(len1)
+# str2 = " sir"
+# len2 = len(str2)
+# print(len2)
+# sum = str1 + str2
+# print(sum) 
+
+# str = "apna college"
+# length = len(str)
+# print(length)
+# srt = str[0:12]
+# print(srt)
+# sft = str.endswith("ee")
+# print(sft)
+
+# str = "helloween"
+# str = str.capitalize()
+# print(str)
+
+# str = "helloween"
+# str = str.replace("o" , "a")
+# print(str)
+# str = "hwllow my name is the cat of time of the universe"
+# str0 = len(str)
+# print (str0)
+# str = str.replace("hwll", "hell")
+# str = str.find("qy")
+# str = str.count("cat")
+# print(str)
+# name = input("name of the user " )
+# rt = len(name)
+# print(rt)
+# str = " hi hero is the $ of the code random"
+# print(str.count("$")) 
+age = int(input("wtf is your age "))
+# age = str(input("what is the age "))
+# if(age == "green"):
+#     print("can vote")
+# elif(age =="red" ):
+#     print("go watch pogo")
+# else:
+#     print("why the fuck you are here" , ",sleep mother fucker")
+# age = float(input("what is the age "))
+# if(age >= 18):
+#     print("can vote")
+# elif(age <= 18 and age >= 1):
+#     print("go watch pogo")
+# elif(age == 0):
+#     print("why the fuck you are here" , ",sleep")
+# else:
+#     print("die")
+# print("this is best for you", print)
+# age = int(input("what is the age "))
+if(age >= 18):
+    if(age >= 80):
+        fuck = "die"
+    else:
+        fuck = "vote"
+elif(age <= 18 and age >= 1):
+    if(age <= 14):
+        fuck = "go watch pogo"
+    else:
+        fuck = "fuck of"
+elif(age == 0):
+    fuck = "why the fuck you are here" , ",sleep"
+else:
+    fuck ="go to hell"
+print("this is best for you ->", fuck)
+# wtf = int(input("is this number is fucked up "))
+# if(wtf % 2 == 0):
+#     yo ="even"
+# elif(wtf % 2 ==1 ):
+#     yo = "odd"
+# print(yo)
+
+# else if elif 
