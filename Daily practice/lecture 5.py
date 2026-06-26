2d94d838f290dc601e4275e41915c0ec60c462d2
Aadi_kun <aaditya27srivastava2009@gmail.com>
2026-06-26 09:07:44 +0530

    Add files via upload

---
+++ Daily practice/lecture 5.py
@@ -0,0 +1,150 @@
+# # count = 1
+# # while count <= 5:
+# #     print("hello", count)
+# #     count += 1 
+# i = 5
+# while i >= 1:
+#     print("hell", i)
+#     i -= 1
+# i = 1 
+# while i <= 100 :
+#     print(i)
+#     i += 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+    
+    
+    
+# i = 1
+# n = int(input("num " ))
+# while i <= 10 :
+#     print (n * i)
+#     i += 1
+
+# i = 1
+# while i <= 10:
+#     print(i+1)
+#     i += 1
+
+# nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]  # useing [0, 2 ] or any formate like this we get the printed value of that specific position 
+# idx = 0
+# while idx < len(nums):
+#     print(nums[idx])
+#     idx += 1
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 81, 100)
+# x = int(input("type the number "))
+# i = 0 
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found" , i )
+#     else:
+#         print("not found")
+# #     i += 1
+# i = 1 
+# while i <= 5:
+#     print(i)
+#     if(i == 3):
+#         break
+#     i += 1
+
+# i = 1
+# while i <= 10:
+#     # print(i)
+#     if(i%2 != 0):      # rule of the odd number
+#         i += 1
+#         continue
+#     print(i)     
+#     i += 1 
+# i = 1
+# while i <=10:
+#     if(i%2 == 0):     # rule of the even number
+#         i += 1
+#         continue
+#     print(i)
+#     i += 1 
+
+# nums = [1, 2, 3, 4, 5]
+# nums = ["potato"]
+# for val in nums:
+#     print(val)
+
+# tup = (1, 3, 4, 5, 6)
+# for num in tup:
+#     print(num)
+
+# str = "aadilovescoffee"
+# y = input("search ")
+# for x in str:
+#     if (x == y):
+#         print("found")
+#         break
+#     print(x)
+# else:
+#     print("end")
+
+list = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+for x in list:
+    print(x, list)
+
+
+# tup = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 25)
+# search = int(input("find the number "))
+# x = 25
+
+# idx = 0 
+# for el in tup:
+#     if(el == x):
+#         print("I am sorry", idx)
+#     idx += 1    
+
+# for i in range(1, 10, 2):
+#     print(i)
+
+# for w in range (2, 21, 2):
+#     print(w)
+
+# for e in range (10, 1, -2):
+#     print(e)
+
+# for n in range(101):
+#     print(n)
+
+# for m in range(100, 0, -1):
+#     print(m)
+
+
+# n = int(input("enter the value "))
+# for i in range(1,11):
+#     print(n * i)
+
+# for i in range(5):
+#     pass
+
+# print("good night")
+
+# if i > 5 :
+#     pass
+# print("wtf")
+
+# n= int(input("put the value "))
+
+# sum = 0 
+# for n in range(1,n+1):
+#     sum += n
+
+# print("the value is ", sum)
+
+# n = 7 
+# sum = 0
+# i = 1
+# while i <= n :
+#     sum += i 
+#     i += 1
+
+    
+# print("the value is ", sum)
