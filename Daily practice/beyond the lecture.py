d7921e914e5240a7717ca78cf8486c1772e30020
Aadi_kun <aaditya27srivastava2009@gmail.com>
2026-06-28 22:14:10 +0530

    Add files via upload

---
+++ Daily practice/beyond the lecture.py
@@ -0,0 +1,55 @@
+# count = int(input("while you = " )) 
+# while count <= 5 :
+#     print("hello544")
+#     count += 1
+# if(count >= 8 and count <= 1):
+#     print("hello")
+# elif(count == 10):
+#     print("mc")
+# else:
+#     print("wtffffff")
+# if (count == 0):
+#     print(" the given")
+
+# nums = (1, 4, 9, 16, 25, 36, 49, 81, 100)
+# x = int(input("type the number "))
+# i = 0 
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print("found" , i )
+#         break
+#     else:
+#         print("not found")
+#     i += 1
+
+# for n in range("im sorry",100):
+#     print(n)
+
+
+
+# i = 1
+# while i <= 100:
+#     print("I am sorry", i)
+#     i += 1
+
+
+    
+# i = 1
+# while i <= 100 :
+#     print("I am sorry ", i )
+#     i += 1
+# a  = 1
+# while a <= 3000:
+#     print("I love you", a)
+#     a+=1
+
+# print("I AM IRON MAN")
+
+
+list =(1,44,5,5,5,5)
+y= list.count(list)
+print(y)
+
+wtf = len(list)
+
+print(wtf)
